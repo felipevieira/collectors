56f2036df733f908b7abfc0a6afbfa5c9cb5106c
Georgiana Bere <bere.georgi@gmail.com>
2016-10-14 22:06:33 +0300

    [#494] Collect only cochrane reviews for publication
    Fixes opentrials/opentrials#494

--- collectors/cochrane_reviews/collector.py
+++ collectors/cochrane_reviews/collector.py
@@ -18,18 +18,20 @@ def collect(conf, conn, date_from=None, date_to=None):
     base.helpers.start(conf, 'cochrane', {})
 
     content = requests.get(conf['COCHRANE_ARCHIVE_URL']).content
+    word_filters = ['published', 'for publication']
     with zipfile.ZipFile(io.BytesIO(content)) as archive:
         for filename in archive.namelist():
-            with archive.open(filename, 'rU') as review_file:
-                file_count += 1
-                db_records = parse_record(conf['COCHRANE_ARCHIVE_URL'], review_file)
-                for rec in db_records:
-                    query = {'file_name': rec['file_name'], 'study_id': rec['study_id']}
-                    if rec.table in conn['warehouse'].tables:
-                        existing = conn['warehouse'][rec.table].find_one(**query)
-                        if existing:
-                            rec['id'] = existing['id']
-                    rec.write(conf, conn)
+            if any(word in filename.lower() for word in word_filters):
+                with archive.open(filename, 'rU') as review_file:
+                    file_count += 1
+                    db_records = parse_record(conf['COCHRANE_ARCHIVE_URL'], review_file)
+                    for rec in db_records:
+                        query = {'file_name': rec['file_name'], 'study_id': rec['study_id']}
+                        if rec.table in conn['warehouse'].tables:
+                            existing = conn['warehouse'][rec.table].find_one(**query)
+                            if existing:
+                                rec['id'] = existing['id']
+                        rec.write(conf, conn)
 
     logger.info("Collected %s review files.", file_count)
     base.helpers.stop(conf, 'cochrane', {'collected': file_count})
